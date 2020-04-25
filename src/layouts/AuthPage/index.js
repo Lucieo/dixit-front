cc7c204643408e6f4987880eb501aba65355de13
LucieO <luciecamilleory@gmail.com>
2020-04-25 22:24:30 +0200

    connect api prod

--- src/layouts/AuthPage/index.js
+++ src/layouts/AuthPage/index.js
@@ -1,46 +1,47 @@
-import React,  {useState} from 'react';
-import Bg from 'images/paintings.jpeg';
-import {ReactComponent as Logo} from 'images/idea.svg';
-import './AuthPage.css';
-import Login from 'layouts/AuthPage/Login';
-import SignUp from 'layouts/AuthPage/Signup';
-import Sloggan from 'components/Sloggan'
+import React, { useState } from "react";
+import Bg from "images/paintings.jpeg";
+import { ReactComponent as Logo } from "images/idea.svg";
+import "./AuthPage.css";
+import Login from "layouts/AuthPage/Login";
+import SignUp from "layouts/AuthPage/Signup";
+import Sloggan from "components/Sloggan";
 
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { ApolloProvider } from "react-apollo";
 
-
 const cache = new InMemoryCache();
 const link = new HttpLink({
- uri: process.env.NODE_ENV === "development" ? 'http://localhost:4000/graphql' : "https://esquisse-api.herokuapp.com/graphql"
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "https://dixit-live.herokuapp.com/graphql",
 });
- 
+
 const client = new ApolloClient({
- cache,
- link
+  cache,
+  link,
 });
 
-
-export default function BasicHome(){
-    const [mode, setMode] = useState('login');
-    return(
-        <ApolloProvider client={client}>
-            <div className="auth-page" style={{background: `url(${Bg})`}}>
-                <Logo className="auth-page__logo"/>
-                <h3 className='auth-page__title'>Dixit Live</h3>
-                <div className="auth-page__action">
-                    <h5 className="center">
-                        <Sloggan/>
-                    </h5>
-                    {
-                        mode==="login"
-                        ? <Login switchMode={()=>setMode('signup')}/>
-                        : <SignUp switchMode={()=>setMode('login')}/>
-                    }
-                </div>
-            </div>
-        </ApolloProvider>
-    )
+export default function BasicHome() {
+  const [mode, setMode] = useState("login");
+  return (
+    <ApolloProvider client={client}>
+      <div className="auth-page" style={{ background: `url(${Bg})` }}>
+        <Logo className="auth-page__logo" />
+        <h3 className="auth-page__title">Dixit Live</h3>
+        <div className="auth-page__action">
+          <h5 className="center">
+            <Sloggan />
+          </h5>
+          {mode === "login" ? (
+            <Login switchMode={() => setMode("signup")} />
+          ) : (
+            <SignUp switchMode={() => setMode("login")} />
+          )}
+        </div>
+      </div>
+    </ApolloProvider>
+  );
 }
--- src/layouts/ConnectedPages/index.js
+++ src/layouts/ConnectedPages/index.js
@@ -1,84 +1,84 @@
-import React from 'react';
-import {
-    HashRouter,
-    Switch,
-    Route,
-} from "react-router-dom";
+import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
-import Home from './Home';
-import Profile from './Profile';
-import Game from './Game';
-import ConnectedPageContainer from 'components/ConnectedPageContainer'
+import Home from "./Home";
+import Profile from "./Profile";
+import Game from "./Game";
+import ConnectedPageContainer from "components/ConnectedPageContainer";
 
-import { ApolloClient } from 'apollo-client';
+import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { split, HttpLink } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/link-ws';
-import {resolvers, localType} from 'graphQL/localState';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/link-ws";
+import { resolvers, localType } from "graphQL/localState";
 
 const cache = new InMemoryCache();
 
 const httpLink = new HttpLink({
-  headers: { authorization: localStorage.getItem('esquisse-token') },
-  uri: process.env.NODE_ENV === "development" ? 'http://localhost:4000/graphql' : "https://esquisse-api.herokuapp.com/graphql"
+  headers: { authorization: localStorage.getItem("esquisse-token") },
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "https://dixit-live.herokuapp.com/graphql",
 });
 
 const wsLink = new WebSocketLink({
-  uri: process.env.NODE_ENV === "development" ? `ws://localhost:4000/graphql` : "wss://esquisse-api.herokuapp.com/graphql",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? `ws://localhost:4000/graphql`
+      : "wss://dixit-live.herokuapp.com//graphql",
   options: {
-    reconnect: true
-  }
+    reconnect: true,
+  },
 });
 
 const link = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
-  httpLink,
+  httpLink
 );
 
 const client = new ApolloClient({
   cache,
   link,
-  credentials: 'include',
-  localType,  resolvers,
-  dataIdFromObject: o=>o.id
+  credentials: "include",
+  localType,
+  resolvers,
+  dataIdFromObject: (o) => o.id,
 });
-
 
 cache.writeData({
   data: {
-    isLoggedIn: !!localStorage.getItem('esquisse-token'),
+    isLoggedIn: !!localStorage.getItem("esquisse-token"),
   },
 });
 
-export default function ConnectedPaged(){
-    return(
-        <ApolloProvider client={client}>
-            <HashRouter>
-              <ConnectedPageContainer>
-                  <Switch>
-                      <Route path="/" exact>
-                          <Home/> 
-                      </Route>
-                      <Route path="/profile">
-                          <Profile/> 
-                      </Route>
-                      <Route 
-                        path="/game/:gameId"
-                      >
-                          <Game/>
-                      </Route>
-                  </Switch>
-              </ConnectedPageContainer>
-            </HashRouter>
-        </ApolloProvider>
-    )
+export default function ConnectedPaged() {
+  return (
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <ConnectedPageContainer>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/game/:gameId">
+              <Game />
+            </Route>
+          </Switch>
+        </ConnectedPageContainer>
+      </HashRouter>
+    </ApolloProvider>
+  );
 }
